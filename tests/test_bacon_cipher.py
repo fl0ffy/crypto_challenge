f2cc774fda9768f400c89a6088ffe6f413659d98
fl0ffy <bernat.johnathan@gmail.com>
2021-02-10 16:07:17 -0500

    removed duplicate function

--- tests/test_bacon_cipher.py
+++ tests/test_bacon_cipher.py
@@ -1,5 +1,5 @@
 import pytest
-from src.bacon import *
+from src.bacon_cipher import *
 
 plaintext = """AAABAABBBAABBABAABBABAAABAAAAABAABBBABAAABABBAAAAABAA
 BBABAAAABBBAABBABBAABA BBAAAABBBABABAA
